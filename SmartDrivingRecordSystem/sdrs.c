b43ba0fddd2a265ea0e9bfb7a5ad7b50c72db2fd
Towkai <ji32k7Email@gmail.com>
2025-04-22 23:45:45 +0800

    [Feature]使用父子處理程序錄製影片

--- SmartDrivingRecordSystem/sdrs.c
+++ SmartDrivingRecordSystem/sdrs.c
@@ -3,7 +3,12 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include "sdrs.h"
+#include "file_upload.h"
+#define FIFO_PATH "/tmp/sdrs_fifo"
 
 int main() {
     time_t rawtime;
@@ -16,37 +21,77 @@ int main() {
     snprintf(mkdir_command, sizeof(mkdir_command), "mkdir -p %s", OUTPUT_DIR);
     system(mkdir_command);
 
-    while (1) {
-        // 取得目前時間
-        time(&rawtime);
-        timeinfo = localtime(&rawtime);
+    // 確保 FIFO 文件存在
+    if (access(FIFO_PATH, F_OK) != 0)
+        mkfifo(FIFO_PATH, 0666);
+    
+    pid_t pid = fork();
+    switch (pid)
+    {
+        case -1:
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+            break;
+        case 0: 
+            while (1) {
+                // 取得目前時間
+                time(&rawtime);
+                timeinfo = localtime(&rawtime);
+        
+                // 產生檔名
+                strftime(filename, sizeof(filename), FILENAME_FORMAT, timeinfo);
+                char full_filename[512];
+                snprintf(full_filename, sizeof(full_filename), "%s/tmp_%s", OUTPUT_DIR, filename);
 
-        // 產生檔名
-        strftime(filename, sizeof(filename), FILENAME_FORMAT, timeinfo);
-        char full_filename[512];
-        snprintf(full_filename, sizeof(full_filename), "%s/tmp_%s", OUTPUT_DIR, filename);
+                // 產生 libcamera-vid 命令
+                snprintf(command, sizeof(command),
+                        "libcamera-vid -t %d000 -o %s --codec h264 --width 1920 --height 1080",
+                        RECORD_DURATION, full_filename);
 
-        // 產生 libcamera-vid 命令
-        snprintf(command, sizeof(command),
-                 "libcamera-vid -t %d000 -o %s --codec h264 --width 1920 --height 1080",
-                 RECORD_DURATION, full_filename);
+                // 執行 libcamera-vid 命令
+                printf("執行命令: %s\n", command);
+                int result = system(command);
+        
+                if (result == 0) {
+                    printf("成功錄製: %s\n", full_filename);
+                    snprintf(command, sizeof(command),
+                            "mv %s %s/%s",
+                            full_filename, OUTPUT_DIR, filename);
+                    int result = system(command);
 
-        // 執行 libcamera-vid 命令
-        printf("執行命令: %s\n", command);
-        int result = system(command);
+                    int fd = open(FIFO_PATH, O_WRONLY);
+                    if (fd == -1) {
+                        perror("open");
+                        exit(1);
+                    }
+                    write(fd, strcat(full_filename, "\n"), strlen(full_filename));
+                    close(fd);
+                } else {
+                    fprintf(stderr, "錄製失敗，返回碼: %d\n", result);
+                }
+        
+                // 等待下一次錄製
+                sleep(1);
+            }
+            break;
+        default:
+            // 以讀取模式打開 FIFO
+            int fd = open(FIFO_PATH, O_RDONLY);
+            if (fd == -1) {
+                perror("open");
+                exit(1);
+            }
+    
+            // 從 FIFO 讀取資料,只讀取第一行
+            char buffer[1024];
+            if (fgets(buffer, sizeof(buffer), fdopen(fd, "r")) == NULL) {
+                perror("fgets");
+                exit(1);
+            }
+            printf("Parent received: %s", buffer);
 
-        if (result == 0) {
-            printf("成功錄製: %s\n", full_filename);
-            snprintf(command, sizeof(command),
-                     "mv tmp_%s %s/%s",
-                     full_filename, OUTPUT_DIR, filename);
-            int result = system(command);
-        } else {
-            fprintf(stderr, "錄製失敗，返回碼: %d\n", result);
-        }
-
-        // 等待下一次錄製
-        sleep(1);
+            wait(NULL);
+            break;
     }
 
     return 0;
--- SmartDrivingRecordSystem/sdrs.h
+++ SmartDrivingRecordSystem/sdrs.h
@@ -1,3 +1,8 @@
+#ifndef SDRS_H
+#define SDRS_H
+
 #define RECORD_DURATION 10
-#define OUTPUT_DIR "~/video" // 請替換成您的硬碟路徑
+#define OUTPUT_DIR "/home/pi/DCIM/Camera" // 請替換成您的硬碟路徑
 #define FILENAME_FORMAT "video_%Y%m%d_%H%M%S.mp4"
+
+#endif // SDRS_H
