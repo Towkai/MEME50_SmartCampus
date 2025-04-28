a5c71a7aeb0cdf42ff46c0d66cd11306729935a6
towkai <towkai@igs.com.tw>
2025-04-28 19:33:18 +0800

    [Modify]格式化輸出接收到的json

--- Nodejs/SmartDrivingRecordSystem/controllers/sdrs_controller.js
+++ Nodejs/SmartDrivingRecordSystem/controllers/sdrs_controller.js
@@ -3,8 +3,8 @@ const sdrs_model = require('../models/sdrs_model');
 const SDRS_Controller = {
     getFiles: async function listFiles(req, res) {
         try {
-            const xmlData = await sdrs_model.getFiles();
-            res.send(`<pre>${xmlData}</pre>`); // 直接顯示原始 XML
+            const Data = await sdrs_model.getFiles();
+            res.send(`<pre>${Data}</pre>`); // 直接顯示原始 XML
         } catch (error) {
             res.status(500).send('Error retrieving files');
         }
--- Nodejs/SmartDrivingRecordSystem/models/sdrs_model.js
+++ Nodejs/SmartDrivingRecordSystem/models/sdrs_model.js
@@ -1,5 +1,6 @@
 // models/userModel.js
 const axios = require('axios');
+const xml2js = require('xml2js');
 const nextcloudUrl = 'http://218.32.100.27:1200/remote.php/dav/files/team-fire/Videos/';
 const username = 'team-fire';
 const password = 'teamfire1234';
@@ -19,7 +20,9 @@ const FileModel = {
                     'Content-Type': 'application/xml' 
                 },
             });
-            return response.data; // 返回原始 XML 響應
+            const parser = new xml2js.Parser();
+            const result = await parser.parseStringPromise(response.data);
+            return JSON.stringify(result, undefined, 2); //prettier json
             
         } catch (error) {
             throw new Error('Error fetching files:', error);
