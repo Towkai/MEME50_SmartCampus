513567bb969b300514643699d680a0d9cda39cf2
towkai <towkai@igs.com.tw>
2025-04-28 14:28:13 +0800

    [Modify]修改專案層級：建立儀表板的資料夾

--- Nodejs/router.js
+++ Nodejs/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('./controller');
+const controller = require('./Dashboard/controllers/main_controller');
 
 router.get('/', controller.index);
 
