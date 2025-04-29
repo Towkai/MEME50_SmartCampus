86e03c00aeac506f5b3c01a071631b855f2e5763
towkai <towkai@igs.com.tw>
2025-04-30 00:01:20 +0800

    [Feature]npm新增模組，cors

--- Nodejs/SmartDrivingRecordSystem/models/sdrs_model.js
+++ Nodejs/SmartDrivingRecordSystem/models/sdrs_model.js
@@ -1,25 +1,23 @@
 // models/userModel.js
-const axios = require('axios');
+const axios = require('axios')
 const xml2js = require('xml2js');
-const nextcloudUrl = 'http://218.32.100.27:1200/remote.php/dav/files/team-fire/Videos/';
-const username = 'team-fire';
-const password = 'teamfire1234';
+const nextcloud = require('../nextcloud');
 
 const FileModel = {
     getFiles: async function getFiles() {
         try {
-            const response = await axios({
-                method: 'PROPFIND',
-                url: nextcloudUrl,
-                auth: {
-                    username: username,
-                    password: password,
-                },
-                headers: { 
-                    'Depth': '1', 
-                    'Content-Type': 'application/xml' 
-                },
-            });
+            const response = await axios(nextcloud.axios_api('PROPFIND'));
+            const parser = new xml2js.Parser();
+            const result = await parser.parseStringPromise(response.data);
+            return result; //prettier json
+            
+        } catch (error) {
+            throw new Error('Error fetching files:', error);
+        }
+    },
+    delFile: async function delFile(file) {
+        try {
+            const response = await axios(nextcloud.axios_api('DELETE', file));
             const parser = new xml2js.Parser();
             const result = await parser.parseStringPromise(response.data);
             return result; //prettier json
