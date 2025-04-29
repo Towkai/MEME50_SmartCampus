6956c6f68ac01a64a8134c3d0e1515ade89ee90a
cindychhuang <keyato12@gmail.com>
2025-04-29 12:14:33 +0800

    [Modify]Parking data更新為絕對路徑後，發現資料夾不在預期的檔案夾中，因此調整路徑

--- Nodejs/Dashboard/controllers/main_controller.js
+++ Nodejs/Dashboard/controllers/main_controller.js
@@ -13,7 +13,7 @@ exports.index = (req, res) => {  //固定用法：建立名為"views"的資料�
 };
 
 exports.Parking= (req, res) => {
-    const publicPath = path.resolve(__dirname, "public");
+    const publicPath = path.resolve(__dirname, "../../public");
     const filePath = path.join(publicPath, "/data/park_data.csv");
     const status = require(path.join(publicPath, "/data/status.json"));
     json = csvToJson.fieldDelimiter(',').getJsonFromCsv(filePath);
