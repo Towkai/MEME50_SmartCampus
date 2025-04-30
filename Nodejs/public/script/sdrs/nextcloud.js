4172a5c8c3ab189f6ce1245ea53118da2b2cb6a3
towkai <towkai@igs.com.tw>
2025-05-01 01:54:04 +0800

    [Feature]儀錶板影片檔案列表背景偷偷參考雲端檔案聶表做修改

--- Nodejs/public/script/sdrs/nextcloud.js
+++ Nodejs/public/script/sdrs/nextcloud.js
@@ -48,3 +48,41 @@ function hideItem(file) {
     let item = document.getElementById(file);
     item.style.display = "none";
 }
+
+function setItem(datas) {
+    let files = Array.from(datas).map(data => data["d:href"][0].split('/').pop());
+    let fileExplorer = document.getElementById("fileExplorer");
+    let ids = Array.from(fileExplorer.children).map(child => child.id);
+    let hideItems = ids.filter(value => !files.includes(value));
+    hideItems.forEach(item => hideItem(item));
+
+    let newItems = datas.filter(data => !ids.includes(data["d:href"][0].split('/').pop()));
+    newItems.forEach(item => ids > 0 ? fileExplorer.insertBefore(newItem(item), fileExplorer.children[0]) : fileExplorer.append(newItem(item)));
+}
+
+function newItem(data) {
+    let href = data["d:href"][0];
+    let isFolder = href.endsWith('/');
+    let item = document.createElement('div');
+    item.id = href.split('/').pop();
+    item.className = `item ${isFolder ? 'folder' : 'file'}`;
+    let span = document.createElement('span');
+    span.className = "itemname";
+    span.innerText = `${isFolder ? '📁' : '📄'} ${href.split('/').pop()}`;
+    item.append(span);
+    let div = document.createElement('div');
+    div.className = "info";
+    span = document.createElement('span');
+    span.innerText = isFolder ? "大小: N/A" : `${data["d:propstat"][0]["d:prop"][0]["d:getcontentlength"][0]}`;
+    div.append(span);
+    span = document.createElement('span');
+    span.innerText = `，最後修改時間: ${data["d:propstat"][0]["d:prop"][0]["d:getlastmodified"][0]}`;
+    div.append(span)
+    let button = document.createElement("button");
+    button.className = "delete-button";
+    button.setAttribute("data-href", href);
+    button.innerText = "刪除";
+    div.append(button);
+    item.append(div);
+    return item;
+}
