bb6f395691e2f9d8acb93083360f8fcdd56f47f7
Towkai <ji32k7Email@gmail.com>
2025-05-01 18:24:31 +0800

    [Modify]儀錶板嵌入Nextcloud

--- Nodejs/Dashboard/controllers/subsystem.js
+++ Nodejs/Dashboard/controllers/subsystem.js
@@ -1,1 +1,1 @@
-exports.urls = ["../parking","../NicoleFoodSystem","../sdrs/getFiles","../sdrs/getFiles","../sdrs/getFiles","../sdrs/getFiles"];
+exports.urls = ["../parking","../NicoleFoodSystem","../sdrs/getFiles","../sdrs/getFiles","../sdrs/getFiles","http://218.32.100.27:1200/s/PFZgyCf9ff2WWzY"];
