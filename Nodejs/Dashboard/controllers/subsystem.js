a3f03c4479d1694ab31d90f8916878e2d6a6d171
sam <sanluxian@gmail.com>
2025-05-02 11:05:37 +0800

    fix subsystem url samfacephoto

--- Nodejs/Dashboard/controllers/subsystem.js
+++ Nodejs/Dashboard/controllers/subsystem.js
@@ -1,1 +1,1 @@
-exports.urls = ["../parking", "../NicoleFoodSystem", "../sdrs/getFiles", "../sdrs/videoPhoto", "../sdrs/getFiles", "http://218.32.100.27:1200/s/PFZgyCf9ff2WWzY"];
+exports.urls = ["../parking", "../NicoleFoodSystem", "../sdrs/getFiles", "../videoPhoto", "../sdrs/getFiles", "http://218.32.100.27:1200/s/PFZgyCf9ff2WWzY"];
