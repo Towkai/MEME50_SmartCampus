9d8e9890986de10c01d64f91ab4b52a675f7c7d0
Towkai <ji32k7Email@gmail.com>
2025-05-02 21:59:00 +0800

    [Modify]修改儀錶板Title [ Dashboard - 智慧校園 ]、新增Nextcloud/Videos超連結

--- Nodejs/Dashboard/controllers/main_controller.js
+++ Nodejs/Dashboard/controllers/main_controller.js
@@ -15,6 +15,7 @@ exports.index = (req, res) => {  //固定用法：建立名為"views"的資料�
         ["feeder", "餵食紀錄"],
         ["SamFacePhoto/videoPhoto", "車輛周邊安全系統"],
         ["sdrs/getFiles", "獲取檔案"],
+        ["http://218.32.100.27:1200/s/PFZgyCf9ff2WWzY", "Nextcloud"],
     ]});
 };
 exports.dashboard = (req, res) => {  //固定用法：建立名為"views"的資料夾，將指定的ejs檔放在裡面
