f188afca18d90679ba636f04f6b77c301e32f705
Towkai <ji32k7Email@gmail.com>
2025-05-16 17:33:08 +0800

    [Modify]更新Nextcloud網址

--- Nodejs/SmartDrivingRecordSystem/nextcloud.js
+++ Nodejs/SmartDrivingRecordSystem/nextcloud.js
@@ -1,4 +1,4 @@
-const url = 'http://218.32.100.27:1200/remote.php/dav/files/team-fire/MEME50/Videos/';
+const url = 'http://218.32.100.27:1200/remote.php/dav/files/team-fire/MEME50/期末專題《智慧校園》/SDRS_Videos/';
 const username = 'team-fire';
 const password = 'teamfire1234';
 function get_full_path(filename) {
--- Nodejs/public/script/sdrs/url.js
+++ Nodejs/public/script/sdrs/url.js
@@ -2,5 +2,5 @@ function url(router, file) {
     return `/sdrs/${router}/${file ?? ''}`;
 }
 function path(file) {
-    return `http://218.32.100.27:1200/remote.php/dav/files/team-fire/MEME50/Videos/${file}`;
+    return `http://218.32.100.27:1200/remote.php/dav/files/team-fire/MEME50/期末專題《智慧校園》/SDRS_Videos/${file}`;
 }
